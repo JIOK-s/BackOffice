c36941d4eb83b4685a6f2e8296cbb43e18d278ca
hyunji-ok <hyunjiok10@gamil.com>
2023-08-19 08:49:32 +0900

    [feat] router object name 설정 및 sidebar 객체 가져오기 수정

--- src/javascript/pages/SidebarMenu.js
+++ src/javascript/pages/SidebarMenu.js
@@ -6,38 +6,17 @@ import { childrenArray } from '../routes/HomeRouter';
 const SidebarMenu = () => {
     return (
         <div>
-            {/*<div>*/}
-            {/*    <ul>*/}
-            {/*        {childrenArray.map((item) => (*/}
-            {/*            <li>{item.path}</li>*/}
-            {/*        ))}*/}
-            {/*    </ul>*/}
-            {/*</div>*/}
             <nav>
                 <ul>
-                    {/*{childrenArray.map((item, index) => {*/}
-                    {/*    <li key={index}>*/}
-                    {/*        <Link to={item.path}>{item.name}</Link>*/}
-                    {/*    </li>;*/}
-                    {/*})}*/}
-                    <li>
-                        <Link to={`/sales`}>sales parent</Link>
-                    </li>
-                    <li>
-                        <Link to={`/sales/1`}>sales child</Link>
-                    </li>
-                    <li>
-                        <Link to={`/members`}>members parent</Link>
-                    </li>
-                    <li>
-                        <Link to={`/members/1`}>members child</Link>
-                    </li>
-                    <li>
-                        <Link to={`/purchases`}>purchases parent</Link>
-                    </li>
-                    <li>
-                        <Link to={`/purchases/1`}>purchases child</Link>
-                    </li>
+                    {
+                        childrenArray.map((item, index) => {
+                            return(
+                                <li key={index}>
+                                    <Link to={item.path}>{item.name}</Link>
+                                </li>
+                            )
+                        })
+                    }
                 </ul>
             </nav>
         </div>
