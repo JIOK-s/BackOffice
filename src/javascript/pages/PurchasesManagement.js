c956adde02e280d7477683102ecb244b606d0890
jinieeee <lje911@gmail.com>
2023-09-02 10:38:50 +0900

    [feat] 구매관리조회 검색조건 개발, checkbox 기능 추가

--- src/javascript/pages/PurchasesManagement.js
+++ src/javascript/pages/PurchasesManagement.js
@@ -6,16 +6,32 @@ import axios from 'axios';
 
 const PurchasesManagement = () => {
     /** const data **/
+    const columnDefs = [
+        {
+            headerName: '유저상품번호',
+            field: 'userProdNo',
+            headerCheckboxSelection: true,
+            checkboxSelection: true,
+        },
+        {
+            headerName: '사용기간',
+            field: 'usedPeriod',
+            editable: true,
+            cellEditor: 'agTextCellEditor',
+            cellEditorParams: {
+                maxLength: 5,
+            },
+        },
+        { headerName: '회원 ID', field: 'mbrId' },
+        { headerName: '회원 전화번호', field: 'mbrPhone' },
+    ];
 
     /** hooks **/
     // useState
     const [rowData, setRowData] = useState([]);
-    const columnDefs = [
-        { headerName: '유저상품번호', field: 'userProdNo' },
-        { headerName: '사용기간', field: 'usedPeriod' },
-        { headerName: '회원 ID', field: 'mbrId' },
-        { headerName: '회원 전화번호', field: 'mbrPhone' },
-    ];
+    const [searchParam, setSearchParam] = useState({});
+    const [selectedRows, setSelectedRows] = useState([]);
+    const [usedPeriodBtnParam, setUsedPeriodBtnParam] = useState([]);
 
     // useMemo
     const containerStyle = useMemo(
@@ -31,14 +47,41 @@ const PurchasesManagement = () => {
     const onRowData = useCallback(() => {
         // Axios를 사용하여 데이터를 가져옵니다.
         axios
-            .get('http://localhost:8080/v1/userProduct/management')
+            .get('http://localhost:8080/v1/userProduct/management', {
+                params: searchParam,
+            })
             .then((response) => {
                 setRowData(response.data);
             })
             .catch((error) => {
                 console.error('Error fetching data:', error);
             });
-    }, []);
+    }, [searchParam]);
+
+    const onRequestParam = useCallback((e) => {
+        const newRequestParam = {
+            ...searchParam,
+            [e.target.name]: e.target.value,
+        };
+        setSearchParam(newRequestParam);
+    });
+
+    const onClickSelectedRows = useCallback((e) => {
+        const newSelectedRows = e.api.getSelectedRows();
+        setSelectedRows(newSelectedRows);
+    });
+
+    const onCellValueChanged = useCallback((e) => {
+        const { colDef, newValue, oldValue, data } = e;
+
+        // TODO: usedPeriodBtnParam 값 추가
+        // const tempParam = {
+        //     userProdNo: data.userProdNo,
+        //     usedPeriod: data.usedPeriod,
+        // };
+        // const newUsedPeriodBtnParam = [...usedPeriodBtnParam, tempParam];
+        // setUsedPeriodBtnParam(newUsedPeriodBtnParam);
+    });
 
     /** jsx **/
     return (
@@ -53,19 +96,49 @@ const PurchasesManagement = () => {
                 {/* 필터 영역 */}
                 <div>
                     <table style={{ border: '1px solid #111111' }}>
+                        <colgroup>
+                            <col style={{ width: '20%' }} />
+                            <col style={{ width: '30%' }} />
+                            <col style={{ width: '20%' }} />
+                            <col style={{ width: '30%' }} />
+                        </colgroup>
                         <thead></thead>
                         <tbody>
                             <tr>
                                 <th>유저상품번호</th>
-                                <td></td>
+                                <td>
+                                    <input
+                                        type={'text'}
+                                        onChange={onRequestParam}
+                                        name={'userProdNo'}
+                                    />
+                                </td>
                                 <th>사용기간</th>
-                                <td></td>
+                                <td>
+                                    <input
+                                        type={'text'}
+                                        onChange={onRequestParam}
+                                        name={'usedPeriod'}
+                                    />
+                                </td>
                             </tr>
                             <tr>
                                 <th>회원 ID</th>
-                                <td></td>
+                                <td>
+                                    <input
+                                        type={'text'}
+                                        onChange={onRequestParam}
+                                        name={'mbrId'}
+                                    />
+                                </td>
                                 <th>회원 전화번호</th>
-                                <td></td>
+                                <td>
+                                    <input
+                                        type={'text'}
+                                        onChange={onRequestParam}
+                                        name={'mbrPhone'}
+                                    />
+                                </td>
                             </tr>
                         </tbody>
                     </table>
@@ -86,9 +159,11 @@ const PurchasesManagement = () => {
                         <AgGridReact
                             rowData={rowData}
                             columnDefs={columnDefs}
-                            rowSelection={'single'}
+                            rowSelection={'multiple'}
                             animateRows={true}
                             getRowId={getRowId}
+                            onRowSelected={onClickSelectedRows}
+                            onCellValueChanged={onCellValueChanged}
                         />
                     </div>
                 </div>
